e8fc4fa59e800eef51f8d5848eddbccec462af61
josuevillagra45 <152455188+josuevillagra45@users.noreply.github.com>
2024-05-07 10:56:47 -0300

    Create ejercicio 7 tp1 villagra.py

---
+++ ejercicio 7 tp1 villagra.py
@@ -0,0 +1,10 @@
+pi = 3.14
+radio = 50
+
+
+def calcular_area_circulo(valor):
+    calculo = pi * (radio * radio)
+    return calculo
+
+resuelvo = calcular_area_circulo("plaza espana")
+print(resuelvo)
